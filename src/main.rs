74c416588bcff8173aecc4780f1f966435ec8dff
Andrew Dibble <andrew@dibble.codes>
2021-11-13 13:28:20 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,125 +1,113 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::result::Result;
+
+type Board = [[u32; 9]; 9];
 
 struct ValidChecker {
-    map: u32,
-    is_valid: bool,
+    map: [bool; 9],
 }
 
 impl ValidChecker {
-    fn new() -> ValidChecker {
-        ValidChecker {
-            map: 0,
-            is_valid: true,
-        }
+    fn new() -> Self {
+        Self { map: [false; 9] }
     }
 
-    fn is_valid(self) -> bool {
-        self.is_valid
-    }
+    fn add(&mut self, num: u32) -> Result<(), ()> {
+        if num != 0 {
+            let value = self
+                .map
+                .get_mut(num as usize - 1)
+                .expect("checker out of bounds");
 
-    fn add(&mut self, num: &u32) {
-        if *num != 0 && self.is_valid {
-            let new_map = self.map | (1 << *num - 1);
-            if self.map == new_map {
-                self.is_valid = false;
-            } else {
-                self.map = new_map;
+            if *value {
+                return Err(());
             }
+
+            *value = true;
         }
+
+        Ok(())
     }
 }
 
-fn is_row_valid(puzzle: &Vec<Vec<u32>>, row_num: usize) -> bool {
+fn is_row_valid(puzzle: &Board, row_num: usize) -> Result<(), ()> {
     let row = &puzzle[row_num];
     let mut checker = ValidChecker::new();
 
     for value in row {
-        checker.add(value)
+        checker.add(*value)?
     }
 
-    return checker.is_valid();
+    Ok(())
 }
 
-fn is_column_valid(puzzle: &Vec<Vec<u32>>, col_num: usize) -> bool {
+fn is_column_valid(puzzle: &Board, col_num: usize) -> Result<(), ()> {
     let mut checker = ValidChecker::new();
 
     for row in puzzle {
-        checker.add(&row[col_num]);
+        checker.add(row[col_num])?
     }
 
-    return checker.is_valid();
+    Ok(())
 }
 
-fn is_square_valid(puzzle: &Vec<Vec<u32>>, col_num: usize, row_num: usize) -> bool {
+fn is_square_valid(puzzle: &Board, col_num: usize, row_num: usize) -> Result<(), ()> {
     let x_start = col_num - (col_num % 3);
     let y_start = row_num - (row_num % 3);
     let mut checker = ValidChecker::new();
 
     for y in y_start..(y_start + 3) {
         for x in x_start..(x_start + 3) {
-            checker.add(&puzzle[y][x]);
+            checker.add(puzzle[y][x])?
         }
     }
 
-    return checker.is_valid();
+    Ok(())
 }
 
-fn load_puzzle() -> Vec<Vec<u32>> {
-    let mut file = File::open("input.txt").unwrap();
+fn load_puzzle() -> Result<Board, std::io::Error> {
     let mut input = String::new();
-    file.read_to_string(&mut input).unwrap();
-    let mut puzzle = vec![vec![0; 9]; 9];
-    let mut pointer: usize = 0;
-    let mut index: usize = 0;
-    for c in input.chars() {
-        if c == '\n' {
-            pointer += 1;
-            index = 0;
-        } else {
-            puzzle[pointer][index] = u32::from(c) - 48;
-            index += 1;
+    File::open("input.txt")?.read_to_string(&mut input)?;
+    let mut puzzle = [[0; 9]; 9];
+    for (row, line) in input.lines().enumerate() {
+        for col in 0..9 {
+            puzzle[row][col] = line[col..col + 1].parse().expect("failed to parse digit");
         }
     }
-    return puzzle;
+
+    Ok(puzzle)
 }
 
-fn solve(puzzle: &mut Vec<Vec<u32>>, cell: usize) -> bool {
-    let row_num = cell / 9;
-    let col_num = cell % 9;
-    let currently_valid = is_row_valid(puzzle, row_num)
-        && is_column_valid(puzzle, col_num)
-        && is_square_valid(puzzle, col_num, row_num);
+fn solve(puzzle: &mut Board, row_num: usize, col_num: usize) -> Result<(), ()> {
+    is_row_valid(puzzle, row_num)?;
+    is_column_valid(puzzle, col_num)?;
+    is_square_valid(puzzle, col_num, row_num)?;
 
-    if !currently_valid {
-        return false;
-    }
-
-    for new_cell in cell..(9 * 9) {
-        let y = new_cell / 9;
-        let x = new_cell % 9;
-
-        if puzzle[y][x] != 0 {
-            continue;
-        }
-
-        for n in 1..=9 {
-            puzzle[y][x] = n;
-            if solve(puzzle, y * 9 + x) {
-                return true;
+    for y in 0..9 {
+        for x in 0..9 {
+            if puzzle[y][x] != 0 {
+                continue;
             }
-            puzzle[y][x] = 0;
-        }
 
-        if puzzle[y][x] == 0 {
-            return false;
+            for n in 1..=9 {
+                puzzle[y][x] = n;
+                if solve(puzzle, y, x).is_ok() {
+                    return Ok(());
+                }
+                puzzle[y][x] = 0;
+            }
+
+            if puzzle[y][x] == 0 {
+                return Err(());
+            }
         }
     }
 
-    return currently_valid;
+    Ok(())
 }
 
-fn print_puzzle(puzzle: &Vec<Vec<u32>>) {
+fn print_puzzle(puzzle: &Board) {
     for row in puzzle {
         for n in row {
             print!("{}", n);
@@ -130,8 +118,8 @@ fn print_puzzle(puzzle: &Vec<Vec<u32>>) {
 }
 
 fn main() {
-    let mut puzzle = load_puzzle();
+    let mut puzzle = load_puzzle().expect("failed to parse puzzle");
     print_puzzle(&puzzle);
-    solve(&mut puzzle, 0);
+    solve(&mut puzzle, 0, 0).expect("failed to solve puzzle");
     print_puzzle(&puzzle);
 }
